e220133aa53a26a7d1a04b4752a68275a2e55cb3
ren <ren096358@mail.freeadwifi.com.tw>
2019-02-13 17:52:04 +0800

    * 完成 1F 九九乘法表

---
+++ multiplication-chart/index.js
@@ -0,0 +1,15 @@
+let vm = new Vue({
+    el: '#app',
+    data: {
+        bord: {
+            amount: 8,
+        },
+        article: {
+            title: '九九乘法表',
+            subtitle: 'MULTIPLICATION CHART',
+        },
+        footer: {
+            text: 'Copyright © 2019 HexSchool. All rights reserved.',
+        }
+    }
+})
